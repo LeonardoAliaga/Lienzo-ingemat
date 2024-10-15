81731ed907a2976cf886fce0ed781d35b72efe36
LeonardoAliaga <aliagaleonardo23@gmail.com>
2024-10-15 18:04:38 -0500

    semi complete responsive design

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,7 @@ window.addEventListener("scroll", function () {
   navbar.classList.toggle("sticky", window.scrollY > 0);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
-
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
+function toggle() {
+  var navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
